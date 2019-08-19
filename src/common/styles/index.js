d0fdd08ba4c110263cc578ca46c95bfd9a723abf
Nguyen The Phung Long (NWA.AT) <LongNTP@fsoft.com.vn>
2019-08-20 01:08:18 +0700

    add common style file

---
+++ src/common/styles/index.js
@@ -0,0 +1,26 @@
+export const SHADOW_BOX = {
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 4
+  },
+  shadowOpacity: 0.3,
+  shadowRadius: 4.65,
+  elevation: 8,
+  borderRadius: 5
+};
+
+export const HEADER_FONT = {
+  fontSize: 16
+};
+
+export const TEXT_FONT = {
+  fontSize: 14
+};
+
+export const FLEX_COMMON = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "row"
+};
